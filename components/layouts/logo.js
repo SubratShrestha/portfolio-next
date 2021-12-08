94fac0e03699c188a01cf76931c5acdafbed6014
Subrat Shrestha <subrat.shrestha3@gmail.com>
2021-12-08 22:33:24 +1100

    working navbar

---
+++ components/layouts/logo.js
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Text, useColorModeValue } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+const LogoBox = styled.span`
+  font-weight: bold;
+  font-size: 20px;
+  display: inline-flex;
+  align-items: center;
+  height: 32px;
+  line-height: 20px;
+  padding: 10px;
+
+  &:hover img {
+    transform: rotate(20deg);
+  }
+`;
+
+const Logo = () => {
+  // const s_logo = `../../public/images/contents/s-logo${useColorModeValue('', '-dark')}.png`;
+  const s_logo = `/images/contents/s-logo${useColorModeValue('', '-dark')}.png`;
+
+  return (
+    <Link href="/">
+      <a>
+        <LogoBox>
+          <Image src={s_logo} width={20} height={20} alt="logo" />
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily="M PLUS Rounded 1c"
+            fontWeight="bold"
+            ml={3}
+          >
+            Subrat Shrestha
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  );
+};
+
+export default Logo;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react';
+
 const Page = () => {
-  return <div>Hey</div>;
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="blue" p={3} mb={6} align="center">
+        Hello
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Subrat Shrestha
+          </Heading>
+          <p>Subheading</p>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Page;
