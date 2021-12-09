dac6ddce7fc69ffa5886b079ae9f049ef16d90da
Subrat Shrestha <subrat.shrestha3@gmail.com>
2021-12-09 19:44:39 +1100

    Added animated section and moved some stuff around in index

---
+++ components/layouts/animatedSection.js
@@ -0,0 +1,19 @@
+import { motion } from 'framer-motion';
+import { chakra, shouldForwardProp } from '@chakra-ui/react';
+
+const StyledDiv = chakra(motion.div, {
+  shouldForwardProp: prop => shouldForwardProp(prop) || prop === 'transition'
+});
+
+const AnimatedSection = ({ children, delay = 0 }) => (
+  <StyledDiv
+    initial={{ y: 10, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.8, delay }}
+    mb={6}
+  >
+    {children}
+  </StyledDiv>
+);
+
+export default AnimatedSection;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,35 @@
 import { Container, Box, Heading, useColorModeValue } from '@chakra-ui/react';
+import AnimatedSection from '../components/layouts/animatedSection';
 
 const Page = () => {
   return (
     <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hey, I'm an aspiring software engineer in Sydney, AUS.
-      </Box>
-
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Subrat Shrestha
           </Heading>
-          <p>Subheading</p>
+          {/* <p>Subheading</p> */}
         </Box>
       </Box>
+
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        mt={6} // maybe
+        align="center"
+      >
+        Hey, I'm an aspiring software engineer in Sydney.
+      </Box>
+
+      <AnimatedSection delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>some work stuff.</p>
+      </AnimatedSection>
     </Container>
   );
 };
