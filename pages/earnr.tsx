1e1078599a3cc25e6c22ca582e5900f4cf199ec5
Subrat Shrestha <subrat.shrestha3@gmail.com>
2022-01-18 12:31:57 +1100

    smaller images, carousel complete

--- pages/earnr.tsx
+++ pages/earnr.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   Flex,
   Heading,
-  HStack,
   Tag,
   Image,
   useColorModeValue,
@@ -11,29 +10,31 @@ import {
 } from '@chakra-ui/react'
 import SwiperCore, { Navigation, Pagination, Scrollbar } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import {EffectCards, Mousewheel} from 'swiper'
 import 'swiper/css/bundle'
 import { NextPage } from 'next'
 import Divider from '../components/layouts/divider'
 import works from '../libs/work'
+import CenteredContainer from '../components/layouts/centeredContainer'
 
-SwiperCore.use([Navigation, Pagination, Scrollbar])
+SwiperCore.use([Navigation, Pagination, Scrollbar, EffectCards, Mousewheel])
 
 const EarnrPage: NextPage = () => {
   const data = works['earnr']
   let images: Array<string> = []
   if (data.numImages) {
     for (let i = 1; i <= data.numImages; i += 1) {
-      images.push(`/images/earnr/earnr-${i}.png`)
+      images.push(`/images/earnr/earnr-${i}.jpg`)
     }
   }
 
   return (
-    <Box height='100%' width='100%' pt={20}>
-      <Flex flexDirection='row' justifyContent='space-between' px={{md: 10, base: 3}}>
-        <Heading as='h1' fontSize="lg">
+    <Box height='100%' width='100%' pt={20} px={{md: 10, base: 3}}>
+      <Flex flexDirection='row' justifyContent='space-between'> 
+        <Heading as='h1' fontSize="2xl">
           {data.title}
         </Heading>
-        <Heading as='h1' fontSize="lg">
+        <Heading as='h1' fontSize="2xl">
           Software Engineer Intern
         </Heading>
       </Flex>
@@ -46,9 +47,9 @@ const EarnrPage: NextPage = () => {
         {data.tags?.map((value, index) => (
           <WrapItem>
             <Tag
-              color={value.textColor}
+              // color={value.textColor}
               key={index}
-              backgroundColor={value.color}
+              // backgroundColor={value.color}
             >
               {value.value}
             </Tag>
@@ -56,15 +57,17 @@ const EarnrPage: NextPage = () => {
         ))}
       </Wrap>
       <Swiper
-        style={{ marginTop: '20px' }}
+        style={{ marginTop: '30px' }}
         spaceBetween={50}
         slidesPerView={1}
         pagination
-        navigation
+        mousewheel
       >
         {images.map((source, i) => (
           <SwiperSlide>
-            <Image borderRadius={10} src={source} alt={`earnr image-${i}`} />
+              <CenteredContainer maxW="100%" height="100%">
+            <Image borderRadius={10} src={source} alt={`earnr image-${i}`} width={{md: "80%", base: "100%"}} />
+            </CenteredContainer>
           </SwiperSlide>
         ))}
       </Swiper>
