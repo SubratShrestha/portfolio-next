fb6b1e0098b5c1c7a15a5f387d91e000bd151ee8
Subrat Shrestha <subrat.shrestha3@gmail.com>
2022-01-19 15:21:37 +1100

    Add defaults for align and justify

--- components/layouts/centeredContainer.tsx
+++ components/layouts/centeredContainer.tsx
@@ -5,15 +5,18 @@ interface CenteredContainerProps {
   children: ReactNode
   maxW: string
   height: string
+  padding: string
+  align?: string
+  justify?: string
 }
 
-const CenteredContainer = ({ children, ...props }: CenteredContainerProps) => {
+const CenteredContainer = ({ children, align="center", justify="center", ...props }: CenteredContainerProps) => {
   return (
     <Container
       display='flex'
       flexDirection='column'
-      justifyContent='center'
-      alignItems='center'
+      justifyContent={justify}
+      alignItems={align}
       {...props}
     >
       {children}
