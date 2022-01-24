03f12c2ff262097df7f0d5b019e011f06d3b99b1
Subrat Shrestha <subrat.shrestha3@gmail.com>
2022-01-24 09:11:42 +0400

    use Roboto mono

--- components/layouts/fonts.tsx
+++ components/layouts/fonts.tsx
@@ -1,16 +1,19 @@
 import { Global } from '@emotion/react'
 
 /*
-* M rounded 1c: @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1C:wght@300;700&display=swap");
-* lato: @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-* */
+ * M rounded 1c: @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1C:wght@300;700&display=swap");
+ * lato: @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+ * source code pro: @import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
+ * Roboto Mono: @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+ * IBM Plex: @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap');
+ * */
 const FontProvider = () => {
   return (
     <Global
       styles={`
-          @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-          * { font-family: "Lato" }
-        `}
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+        * { font-family: "Roboto Mono" }
+      `}
     />
   )
 }
